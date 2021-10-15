475ae8a0ea26cbdef9e737e035aff247780d25ef
IgorHenrique88 <igorhenrique307@gmail.com>
2021-10-15 16:35:46 -0300

    Commit dia 15/10/2021 às 16:35

--- db/inserir.php
+++ db/inserir.php
@@ -1,1 +1,17 @@
-<div class="titulo">Inserir Valor</div>
+<div class="titulo">Inserir Registro #01</div>
+<?php 
+
+require_once "conexao.php";
+
+$sql ="INSERT INTO cadastro (nome, nascimento, email, site, filhos, salario)
+       VALUES ('Marieta', '1989-10-29', 'marieta123@email.com.br', 'https://marieta.sites.com.br', 2, 13000.78)";
+
+$conexao = novaConexao();
+$resultado = $conexao-> query($sql);
+
+if($resultado) {
+    echo "Sucesso :D!";
+} else {
+    echo "Error: " . $conexao->error;
+}
+$conexao->close();
