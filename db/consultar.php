99fb4fe5ed1e3350d5d2d13f71fe274f4f7bab04
IgorHenrique88 <igorhenrique307@gmail.com>
2021-10-18 16:07:09 -0300

    Inserção consulta.php

--- db/consultar.php
+++ db/consultar.php
@@ -1,3 +1,43 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <div class="titulo">Consultar Registros</div>
-<?php
 
+
+<?php
+require_once "conexao.php";
+
+$sql = "SELECT id, nome, nascimento, email FROM cadastro";
+
+$conexao = novaConexao();
+$resultado = $conexao->query($sql);
+
+$registros = [];
+
+if($resultado->num_rows > 0){
+    while($row = $resultado->fetch_assoc()){
+        $registros[] = $row;
+    }
+} elseif($conexao->error) {
+    echo "Erro: ". $conexao->error;
+}
+
+$conexao->close();
+?>
+
+<table class="table table-hover">
+    <thead>
+        <th>ID</th>        
+        <th>Nome</th>        
+        <th>Nascimento</th>
+        <th>E-mail</th>
+    </thead>
+    <tbody>
+        <?php foreach($registros as $registro): ?>
+            <tr>
+                <td><?= $registro['id'] ?></td>
+                <td><?= $registro['nome'] ?></td>
+                <td><?= $registro['nascimento'] ?></td>
+                <td><?= $registro['email'] ?></td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
